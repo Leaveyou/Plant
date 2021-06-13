01a0dd8655f1704ceb01ec733f7f11fca6b29196
Liviu Gelea <liviu.gelea@gmail.com>
2021-06-14 01:09:38 +0300

    just saving progresss

--- planta.ino
+++ planta.ino
@@ -3,16 +3,31 @@
 #include <DS3231.h>
 #include <EEPROM.h>
 
-#define HOUR_INTERVAL 0
-#define MINUTE_INTERVAL 0
-#define SECOND_INTERVAL 10
-#define UNIX_INTERVAL (HOUR_INTERVAL * 3600 + MINUTE_INTERVAL * 60 + SECOND_INTERVAL)
+#define AWAKE_PIN 13  // Powers peripherials. set via [un]unpower_peripherials() function
+#define EEPROM_DATA_VERSION 4 // increment this every type you make changes to the eeprom structure
 
-const byte interruptPin = 2;
-int awakePin = 13;
-long lastCheck = 0;
+// store settings on eeprom 
+typedef struct settings {
+    uint8_t data_version   = EEPROM_DATA_VERSION % 256;
+    uint8_t checks_stored  = 10;
+    uint8_t pump_seconds   = 5;
+    uint8_t check_interval = 12; 
+    uint16_t humidity_threshold = 50;
+} settings;
 
+// store checks on eeprom
+typedef struct check {
+    uint32_t timestamp = 0;
+    uint16_t humidity = 0;
+    bool ran = 0;
+} check;
+
+
+settings CurrentSettings;
 RTClib myRTC;
+uint16_t eeprom_write_address = 0;
+
+
 
 
 // to do: customizable time interval / water pump
@@ -21,19 +36,15 @@ RTClib myRTC;
 // implementeaza buton scos din sleep cu encoder de rotatie
 // alarmă pt "nu pare sa se fi udat planta cand trebuia" - scrie eroare pe eeprom si afiseaza la apasare buton
 
+
 void setup() 
 {
     Serial.begin(115200);    
-    pinMode(awakePin, OUTPUT);
+    pinMode(AWAKE_PIN, OUTPUT);
     power_peripherials();
-    pinMode(interruptPin, INPUT_PULLUP);
     Wire.begin();
 
-
-//    for(int i = 0; i<512; i++) {
-//      EEPROM.write(i, 0);
-//    }
-
+    CurrentSettings = GetCurrentSettings();
 }
 
 void loop() {
@@ -42,14 +53,17 @@ void loop() {
     long last_check = getLastCheck();
     long time_since_check = current_time - last_check;
 
-    if (time_since_check < UNIX_INTERVAL)
+    uint16_t check_interval_seconds = CurrentSettings.check_interval * 3600; //time always processed in seconds for unixtime
+    if (time_since_check < check_interval_seconds) 
     {
         unpower_peripherials();
-        nipsleep(UNIX_INTERVAL - time_since_check, 0, 0);
+        nipsleep(check_interval_seconds - time_since_check, 0, 0);
         power_peripherials();
     }
        
-    check();
+    wakeup_routine();
+
+    
     delay(1000);
 }
 
@@ -69,31 +83,51 @@ void nipsleep(long seconds, long minutes, long hours)
     }
 }
 
-void check()
+void log_watering()
 {
-    // todo implement
+    uint32_t current_time = get_time(); 
+    // todo: everything below
+}
+
+
+void water_plant() 
+{
+    // todo: implement    
+}
+
+int getHumidity()
+{
+    return 250;
+    // todo: implement
+}
+
+void wakeup_routine()
+{
+    uint8_t humidity = getHumidity();
+    uint32_t timestamp = get_time();
+    
+    if (humidity < CurrentSettings.humidity_threshold) {
+        water_plant();
+    }
+    
+    log_watering();
 }
 
 long getLastCheck()
 {
-    if (!lastCheck){
-        Serial.println("Last check time stale. getting from eeprom...");
-        EEPROM.get(0, lastCheck);
-    }
-    
-    return lastCheck;
+    // todo: implement
 }
 
 
 void power_peripherials()
 {
-    digitalWrite(awakePin, HIGH);
+    digitalWrite(AWAKE_PIN, HIGH);
     delay(1);
 }
 
 void unpower_peripherials()
 {
-    digitalWrite(awakePin, LOW);
+    digitalWrite(AWAKE_PIN, LOW);
     delay(1);
 }
 
@@ -101,4 +135,33 @@ uint32_t get_time()
 {
     DateTime now = myRTC.now();
     return now.unixtime();
+}
+
+settings GetCurrentSettings()
+{
+    settings eeprom_settings;
+    EEPROM.get(0, eeprom_settings);
+    if (eeprom_settings.data_version == EEPROM_DATA_VERSION)
+    {
+        return eeprom_settings;
+              
+    }
+    settings default_settings;
+    FlashEEPROM(default_settings);
+}
+
+void FlashEEPROM(settings system_settings)
+{
+    Serial.println("Flashing EEPROM for version %d... ", EEPROM_DATA_VERSION);
+    check default_check;
+    EEPROM.put(0, system_settings);    
+
+    for (size_t i = 0; i < system_settings.checks_stored; i++)
+    {
+        size_t check_address = sizeof(system_settings) + i*sizeof(check);
+        EEPROM.put(check_address, default_check); 
+        Serial.println(check_address);
+    }
+    eeprom_write_address = sizeof(system_settings);
+    Serial.print(" done");
 }
